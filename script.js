12ae901e973a96c2d54c8e10aec0c53bfcffdd97
Smruthi <138140448+cupoglee@users.noreply.github.com>
2023-07-03 16:36:34 +0530

    Day 3
    Third day learnings of javascript along with some parts of the second and first days.

---
+++ script.js
@@ -0,0 +1,23 @@
+ let js = "amazing";
+console.log(22 + 13);
+console.log(23);
+let firstName = 'smruie'; //Declaring a variable
+console.log(first); 
+
+ console.log(false);
+ let javaScriptThingy= true;
+ console.log(javaScriptThingy);
+ console.log(typeof true);
+ console.log(typeof 23);
+ console.log(typeof 'smruie');
+ javaScriptThingy = 'yes!';
+ console.log(javaScriptThingy);  //dynamic typing
+ let shruk;
+ console.log(shruk); //undefined
+ console.log(typeof shruk); //undefined
+ shruk=2005;
+ console.log(typeof shruk);
+ console.log(typeof null); 
+
+
+ 
