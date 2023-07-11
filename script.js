e9b1c9a870813e50a09595e1a2f2002e1eebb756
Smruthi <138140448+cupoglee@users.noreply.github.com>
2023-07-11 12:34:48 +0530

    Day 4+5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- let js = "amazing";
+/* let js = "amazing";
 console.log(22 + 13);
 console.log(23);
 let firstName = 'smruie'; //Declaring a variable
@@ -18,6 +18,43 @@ console.log(first);
  shruk=2005;
  console.log(typeof shruk);
  console.log(typeof null); 
+*/
+//let age=30;
+//age=31; //reassigning
+
+//const birthYear=1990;
+// birthYear=2005; -----> const is immutable
+//const yeah; //------> this is illegal
+//var hiIAmSmruie= 'programmer';
+//hiIAmSmruie='coder';  //Never write a variable like this without actuall declaring it.
+//console.log(hiIAmSmruie); 
+
+//Math Operators
+const shit= 2037;
+const ageSmruie = shit - 2005;
+const ageSarah = shit - 1991;
+console.log(ageSmruie, ageSarah);
+
+console.log(ageSmruie + 3, ageSmruie / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3
+
+const firstName = 'Smruthi';
+const lastName = 'Kadagadkai';
+console.log(firstName + " " + lastName);
+
+//Assignment Operators
+let x= 10+5;
+x+=10; //x=x+10;
+x*=4; // x=x*4;
+x++; //x = x+1
+console.log(x);
+
+//Comparison Operators
+console.log(ageSmruie > ageSarah); //boolean kind of result
+console.log(ageSmruie >= 18);
 
 
- 
+
+
+
+
