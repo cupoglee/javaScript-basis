60e5f6a2f5cea148047620cf20886702a37ae7e4
Smruthi <138140448+cupoglee@users.noreply.github.com>
2023-07-11 12:38:58 +0530

    JS Assignment

---
+++ assignment.js
@@ -0,0 +1,21 @@
+// Assignment 1 - Values and Variables 
+const country="India";
+const continent='asia';
+let population= 1400000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Assignment 2 - Data Types
+let isIsland = 'Andaman and Nicobar';
+//let isIsland = 'false';
+let language;
+console.log(isIsland); //Uncaught SyntaxError: Identifier 'isIsland' has already been declared 
+
+//Assignment 3 - let, const and var
+language = 'Kannada';
+console.log(language);
+
+//Assignment 4 - Basic Operators
+
+
